87c667dca6b3c7e5c93639787b3a6e7ead126110
Simmy Pappachen <sspnyc@gmail.com>
2015-05-26 18:28:21 -0400

    fixingPhotos

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,11 @@
 class ImagesController < ApplicationController
 
 	def index #displays all items
-		@images = Image.find_by category: "Walter"
+# puts @images		
+@walter = Image.where(:category => 'Walter')
+@baby = Image.where(:category => 'Baby')
+@blue_sky = Image.where(:category => 'Blue_sky')
+@gus = Image.where(:category => 'Gus')
 	end
 	
 
